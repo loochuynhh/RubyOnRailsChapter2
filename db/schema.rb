9bac8ff5f16c876876934911117745f70b39f019
loochuynhh <lochuynhhsb@gmail.com>
2024-09-09 14:13:55 +0700

    Comment some testcase for upload code without fail (temp)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_09_030659) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_09_063649) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
